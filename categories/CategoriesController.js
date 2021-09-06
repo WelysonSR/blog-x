5e2b1db563dbec6ea1315ba0bfe035cd72254d34
Welyson Dos Santos Rosa <welysonsr@gmail.com>
2021-09-06 17:40:54 -0300

    Implementing category listing and implementing the delete method

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -4,10 +4,32 @@ const Category = require("./Category");
 const slugify = require("slugify");
 
 //Routes GET
+router.get("/admin/categories", (req, res) =>{
+    Category.findAll().then(categories => {
+        res.render("admin/categories/index", {categories: categories});
+    });    
+});
+
 router.get("/admin/categories/new", (req, res) =>{
     res.render("admin/categories/new");
 });
 
+router.get("/categories/delete/:id", (req, res) =>{
+    let id = req.params.id
+    //let id = req.body.id;
+
+    if(id != undefined && !isNaN(id)){
+        Category.destroy({
+            where:{
+                id:id
+            }
+        }).then(() => {
+            res.redirect("/admin/categories");
+        });
+    }else{
+        res.redirect("/admin/categories");
+    }
+});
 //Routes POST
 router.post("/categories/save", (req, res) =>{
     let title = req.body.title;

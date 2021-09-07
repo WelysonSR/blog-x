7d498ea0c3e8a0be562df6076eb2b7bff7d49def
Welyson Dos Santos Rosa <welysonsr@gmail.com>
2021-09-07 17:18:58 -0300

    Inserting tinymce

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -1,15 +1,38 @@
 const express = require('express');
 const router = express.Router();
+const Category = require("../categories/Category");
+const Article = require("./Article");
+const slugify = require("slugify");
 
 //Routes GET
-router.get("/articles", (req, res) =>{
-    res.send("Rota de artigos");
+router.get("/admin/articles", (req, res) =>{
+    res.send("Artigos");   
 });
 
 router.get("/admin/articles/new", (req, res) =>{
-    res.render("admin/articles/new");
+    Category.findAll().then(categories =>{
+        res.render("admin/articles/new",{categories: categories});
+    })    
 });
 
 //Routes POST
+
+router.post("/articles/save", (req, res) =>{
+    let title = req.body.title;
+    let body = req.body.body;
+    let category = req.body.category;
+
+    console.log(title);
+    console.log(category);
+    res.redirect("/admin/articles");
+    Article.create({
+        title: title,
+        slug: slugify(title),
+        body: body,
+        categoryId: category
+    }).then(()=>{
+        res.redirect("/admin/articles");
+    });
+});
 
 module.exports = router;

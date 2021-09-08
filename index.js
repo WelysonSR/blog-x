e9a99f1aaa4d9dac6953255b13a90625ff3abf68
Welyson Dos Santos Rosa <welysonsr@gmail.com>
2021-09-08 14:10:48 -0300

    Desenvolvendo a pagina inicial, listando artigos publicado e crando pagina leia mais

--- index.js
+++ index.js
@@ -21,7 +21,29 @@ connection
 
 /******************Routes******************/
 app.get("/",(req,res)=>{
-    res.render("index");
+    Article.findAll({
+        order:[
+            ['id','DESC']
+        ]
+    }).then(articles => {
+        res.render("index",{articles: articles});
+    });
+});
+app.get("/:slug",(req, res)=>{
+    let slug = req.params.slug;
+    Article.findOne({
+        where: {
+            slug: slug
+        }
+    }).then(article =>{
+        if(article != undefined){
+            res.render("article",{article: article});
+        }else{
+            res.render("/");
+        }
+    }).catch(erro => {
+        res.render("/");
+    });
 });
 app.use("/", categoriesController);
 app.use("/", articlesController);

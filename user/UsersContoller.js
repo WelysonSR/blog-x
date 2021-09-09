d91945dcf120fccfec87d9eb1b923dc5fbebe058
Welyson Dos Santos Rosa <welysonsr@gmail.com>
2021-09-09 19:04:29 -0300

    looking up user in the database and listing registered users

--- user/UsersContoller.js
+++ user/UsersContoller.js
@@ -1,13 +1,43 @@
 const express = require('express');
 const router = express.Router();
 const User = require("./User");
+const bcrypt = require("bcryptjs");
 
+//Rotas Get
 router.get("/admin/users",(req,res)=>{
-
+    User.findAll().then(users =>{
+        res.render("admin/users/index",{users: users});
+    });
 });
 
 router.get("/admin/users/create",(req,res)=>{
     res.render("admin/users/create");
+});
+
+//Rotas Post
+router.post("/users/create", (req, res) => {
+    let name = req.body.name;
+    let email = req.body.email;
+    let password = req.body.password;
+
+    User.findOne({ where: { email: email } }).then(user => {
+        if (user == undefined) {
+            let salt = bcrypt.genSaltSync(10);
+            let hash = bcrypt.hashSync(password, salt);
+
+            User.create({
+                name: name,
+                email: email,
+                password: hash
+            }).then(() => {
+                res.redirect("/admin/users/create");
+            }).catch(err => {
+                res.redirect("/admin/users/create");
+            });
+        } else {
+            res.redirect("/");
+        }
+    });
 });
 
 module.exports = router;
